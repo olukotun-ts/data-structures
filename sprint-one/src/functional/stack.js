f16e39ffe5bbf1dcce8f399721768d080604a19a
Sam Olukotun <otsam88@gmail.com>
2017-03-29 16:09:08 -0700

    Implementation in Functional style.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,24 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+
+
   // Implement the methods below
   someInstance.push = function(value) {
+    var index = someInstance.size();
+    storage[index] = value;
+
   };
 
   someInstance.pop = function() {
+    var index = someInstance.size()-1;
+    var temp = storage[index];
+    delete storage[index];
+    return temp;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
