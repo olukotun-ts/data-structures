52dfc88a7a27419604c775ccbfa1761ef5723ea5
Sam Olukotun <otsam88@gmail.com>
2017-03-29 20:25:56 -0700

    Implements Functional-Shared style.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,50 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  someInstance.storage = {};
+
+
+
+  // connect
+  // extend(someInstance, stackMethods);
+
+  someInstance.push = stackMethods.push;
+  someInstance.pop = stackMethods.pop;
+  someInstance.size = stackMethods.size;
+
+  //console.log(someInstance);
+
+  return someInstance;
 };
+
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
 
 var stackMethods = {};
 
 
+  // Implement the methods below
+  stackMethods.push = function(value) {
+    var index = this.size();
+    this.storage[index] = value;
+  };
+
+
+
+
+  stackMethods.pop = function() {
+    var index = this.size()-1;
+    var temp = this.storage[index];
+    delete this.storage[index];
+    return temp;
+  };
+
+  stackMethods.size = function() {
+    return Object.keys(this.storage).length;
+  };
