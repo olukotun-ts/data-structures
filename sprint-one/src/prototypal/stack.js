b4d4b3a6d89d5b25303d5f8806da3fee2bf360ad
Sam Olukotun <otsam88@gmail.com>
2017-03-30 10:10:55 -0700

    Meets spec.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,22 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function (val){
+  this.storage[this.size()] = val;
+}
 
+stackMethods.pop = function (){
+  var temp = this.storage[this.size() - 1];
+  delete this.storage[this.size() - 1];
+  return temp;
+}
+
+stackMethods.size = function (){
+  return Object.keys(this.storage).length;
+}
