592ada31e25b5891e7ce99913e504a93e7438203
Sam Olukotun <otsam88@gmail.com>
2017-03-30 10:56:03 -0700

    Meets spec.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.start = 0;
+  this.finish = 0;
 };
 
+Queue.prototype.enqueue = function (val){
+  this.storage[this.finish] = val;
+  this.finish++;
+};
 
+Queue.prototype.dequeue = function (){
+  if (this.size() > 0){
+    var temp = this.storage[this.start];
+    delete this.storage[this.start];
+    this.start++;
+    return temp;
+  }
+};
+
+Queue.prototype.size = function (){
+  return this.finish - this.start;
+};
