18b60ab49a768f95f62ea9ebb15491d37ca48832
Sam Olukotun <otsam88@gmail.com>
2017-03-29 20:27:45 -0700

    Implemented Functional-Shared style.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,39 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.start = 0;
+  someInstance.finish = 0;
+
+  // Extend queueMethods.
+  extend(someInstance, queueMethods);
+
+  return someInstance;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+  this.storage[this.finish] = value;
+  this.finish++;
+};
 
+queueMethods.dequeue = function(){
+  if (this.finish - this.start > 0){
+    var temp = this.storage[this.start];
+    delete this.storage[this.start];
+    this.start++;
+    return temp;
+  }
+};
+
+queueMethods.size = function(){
+  return this.finish - this.start;
+};
