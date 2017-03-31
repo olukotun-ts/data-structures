fd6f87e85a214c350ba9fa6ab468696d2c7154bf
Sam Olukotun <otsam88@gmail.com>
2017-03-31 11:28:37 -0700

    Pseudo. Outlined strategy.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,18 +2,30 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+
+  newTree.children = [];  //implement children as an array trees
+
+  // extend function goes here
 
   return newTree;
 };
 
+
+// add extend method
+
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // create a new tree
+  // set the value property of the child tree to value
+  // add to the children array
 };
 
 treeMethods.contains = function(target) {
+  // create var to hold results
+  // base case - check if value of current tree is target
+
+  // for each child check if child value is target
 };
 
 
