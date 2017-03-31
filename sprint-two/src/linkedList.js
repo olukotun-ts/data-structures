6c42e1d16a6046e422a9d0033c133d81751b0e05
Sam Olukotun <otsam88@gmail.com>
2017-03-30 19:52:51 -0700

    Implemented .addToTail(), .removeHead().

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,27 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+
+    if (list.head === null){
+      list.head = newNode;
+      list.tail = newNode;
+      return;
+    }
+    list.tail.next = newNode;
+    list.tail = newNode;
+    return;
   };
 
   list.removeHead = function() {
+    if (list.head !== null){
+      // get the next node
+      var temp = list.head;
+      // set the next node
+      list.head = list.head.next;
+      return temp.value;
+
+    }
   };
 
   list.contains = function(target) {
